21fb6278d7e6948cd16c428add97001107f99ebc
Rishabh Kaushik <rishabhrkaushik@gmail.com>
2024-10-29 18:03:24 -0500

    added i2c slave, import it in project

--- src/project.v
+++ src/project.v
@@ -4,6 +4,7 @@
  */
 
 `default_nettype none
+`include "i2c_slave.v"
 
 module tt_um_bsrk_i2c_calc (
     input  wire [7:0] ui_in,    // Dedicated inputs
@@ -16,10 +17,21 @@ module tt_um_bsrk_i2c_calc (
     input  wire       rst_n     // reset_n - low to reset
 );
 
+  wire SDA, SCL, RST;
+  assign SDA = uio_in[0];
+  assign SCL = uio_in[1];
+  assign RST = uio_in[2];
+
+  i2c i2c_peripheral (
+      .SDA  (SDA),    // Dedicated inputs
+      .SCL (SCL),   // Dedicated outputs
+      .RST (RST)   // IOs: Input path
+  );
+
   // All output pins must be assigned. If not used, assign to 0.
-  assign uo_out  = ui_in + uio_in;  // Example: ou_out is the sum of ui_in and uio_in
-  assign uio_out = 0;
-  assign uio_oe  = 0;
+  // assign uo_out  = ui_in + uio_in;  // Example: ou_out is the sum of ui_in and uio_in
+  // assign uio_out = 0;
+  // assign uio_oe  = 0;
 
   // List all unused inputs to prevent warnings
   wire _unused = &{ena, clk, rst_n, 1'b0};
